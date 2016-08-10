89f75d16ecb4b75e3fa1e434eddbde8f52b78506
Klesomik <neon_76@mail.ru>
2016-08-10 22:03:23 +0300

    Added some constants
    Added COLORREF constants

--- Centre.cpp
+++ Centre.cpp
@@ -10,7 +10,7 @@ struct Point
 };
 
 int Init (const int size_x, const int size_y);
-void SetPoints (std::vector <Point>& data, const int size_x, const int size_y);
+void SetPoints (std::vector <Point>& data, const int size_x, const int size_y, const COLORREF information, const COLORREF centre);
 void Calculate (const std::vector <Point>& data, Point& middle, const size_t j);
 void Clear (const int x1, const int y1, const int x2, const int y2);
 void Sleep (const Point& current);
@@ -20,6 +20,8 @@ int main ()
 {
     try
     {
+        const COLORREF information = TX_WHITE, centre = TX_YELLOW;
+
         const int size_x = 800, size_y = 600;
 
         int size = Init (size_x, size_y);
@@ -28,7 +30,7 @@ int main ()
 
         std::vector <Point> data (size);
 
-        SetPoints (data, size_x, size_y);
+        SetPoints (data, size_x, size_y, information, centre);
 
         Point middle;
 
@@ -37,14 +39,14 @@ int main ()
 
         middle.weight = 5;
 
-        txSetColor (TX_YELLOW);
-        txSetFillColor (TX_YELLOW);
+        txSetColor (centre);
+        txSetFillColor (centre);
 
         txCircle (middle.coords[0], middle.coords[1], middle.weight);
 
         Clear (size_x - 150, size_y - 50, size_x, size_y);
 
-        txSetColor (TX_WHITE);
+        txSetColor (information);
 
         adTextOut (size_x - 150, size_y - 50, "(%d, %d, %d)", middle.coords[0], middle.coords[1], middle.weight);
     }
@@ -73,7 +75,7 @@ int Init (const int size_x, const int size_y)
     return size;
 }
 
-void SetPoints (std::vector <Point>& data, const int size_x, const int size_y)
+void SetPoints (std::vector <Point>& data, const int size_x, const int size_y, const COLORREF information, const COLORREF centre)
 {
     const int none = 0, left = 1, range = 255;
 
@@ -87,7 +89,7 @@ void SetPoints (std::vector <Point>& data, const int size_x, const int size_y)
 
                 COLORREF color = RGB (random (range), random (range), random (range));
 
-                while (color == TX_WHITE || color == TX_YELLOW)
+                while (color == information || color == centre)
                     color = RGB (random (range), random (range), random (range));
 
                 for (; txMouseButtons () != none; data[i].weight++)
@@ -96,7 +98,7 @@ void SetPoints (std::vector <Point>& data, const int size_x, const int size_y)
 
                     Clear (size_x - 150, size_y - 50, size_x, size_y);
 
-                    txSetColor (TX_WHITE);
+                    txSetColor (information);
                     txSelectFont ("Comic Sans MS", 28);
 
                     adTextOut (size_x - 150, size_y - 50, "(%d, %d, %d)", data[i].coords[0], data[i].coords[1], data[i].weight + 1);
